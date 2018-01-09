c1c335c9e72c037751234111dcb5afe14863d1dd
yjiong@msn.com <yjiong@msn.com>
2018-01-09 10:08:10 +0800

    fix update-sh

--- update-sh/rmup.sh
+++ update-sh/rmup.sh
@@ -5,23 +5,36 @@
 # mail: 4418229@qq.com
 # Created Time: 2017-11-14 11:34:10
 #########################################################################
-if [ -z $0 ] || [ -z $2 ]
-then echo "Usage:$0 username passwd"
-    exit 1
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
+then echo "Usage:$0 username passwd filename"
+    USER=root
+    PW=93010005
+    FILE=tg150
+    #exit 1
+else
+    USER=$1
+    PW=$2
+    FILE=$3
 fi
+
 echo -e "更新结果\r" >updatelog.txt
 for ip in `cat ipaddress.txt`
 do  
     if [[ $ip =~ ^#.* ]];then
         continue
         fi
-./autossh.exp $ip $1 $2 stop
+./autossh.exp $ip ${USER} ${PW} stop
 ret=$?
 if [ $ret -ne 0 ];then 
-    echo  "connect $ip field " >>updatelog.txt
+    echo  "connect $ip failed " >>updatelog.txt
 else
-./autoscp.exp $ip $1 $2 tg150
-./autossh.exp $ip $1 $2 start 
-    echo  "$ip update successful " >>updatelog.txt
+    ./autoscp.exp $ip ${USER} ${PW} ${FILE}
+    retscp=$?
+    ./autossh.exp $ip ${USER} ${PW} start 
+    if [ $retscp -ne 0 ];then 
+        echo  "$ip update failed " >>updatelog.txt
+    else
+        echo  "$ip update successful " >>updatelog.txt
+    fi
 fi
 done
